1c03899f462b235298043ff3deb3d187e5450476
Noel Merket <noel.merket@nrel.gov>
2017-06-02 16:02:36 -0600

    updating version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='hescore-hpxml',
-    version='3.0.1',
+    version='3.1',
     description='HPXML Translator for the HEScore API',
     long_description=long_description,
     url='https://github.com/NREL/hescore-hpxml',
