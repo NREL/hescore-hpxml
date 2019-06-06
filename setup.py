4f1ea2e7ebb1b4f4c8323b832acb798162cf6c67
Noel Merket <noel.merket@nrel.gov>
2019-06-06 16:29:42 -0600

    removing python 3.5 support

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering',
