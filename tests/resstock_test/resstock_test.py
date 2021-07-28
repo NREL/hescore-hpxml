75b36f455ed6f19c61b16d3fb4895d6a8dab7c1a
aspeake1 <andrew.speake@nrel.gov>
2021-07-28 09:39:48 -0600

    expand resstock testing

--- tests/resstock_test/resstock_test.py
+++ tests/resstock_test/resstock_test.py
@@ -9,12 +9,18 @@ xml_path = os.path.join(thisdir, 'xmls')
 out_path = os.path.join(thisdir, 'output')
 if not os.path.exists(out_path):
     os.makedirs(out_path)
-for file in os.listdir(out_path):
-    os.remove(os.path.join(out_path, file))
+for subdir, dirs, files in os.walk(out_path):
+    for file in files:
+        os.remove(os.path.join(subdir, file))
 
-for xml in os.listdir(xml_path):
-    if xml.split('.')[-1] != 'xml': continue
-    print(f"Running {xml}")
-    file_name = xml.split('.')[0]
-    command = 'hpxml2hescore ' + os.path.join(xml_path, xml) + ' --resstock ' + '-o ' + os.path.join(out_path, file_name + '.json')
-    os.system(command)
+for subdir, dirs, files in os.walk(xml_path):
+    sub_out_path = os.path.join(out_path, subdir.split('\\')[-1])
+    if not os.path.exists(sub_out_path):
+        os.makedirs(sub_out_path)
+    for filename in files:
+        if filename.split('.')[-1] != 'xml' : continue
+        filepath = subdir + os.sep + filename
+        print(f"Translating {subdir}\{filename}")
+        file_name = filename.split('.')[0]
+        command = 'hpxml2hescore ' + filepath + ' --resstock ' + '-o ' + os.path.join(sub_out_path, file_name + '.json')
+        os.system(command)
