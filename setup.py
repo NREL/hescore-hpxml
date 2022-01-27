7cfcff4356bc8ca4a4dcf63dea9866659d049e3b
bpark1327 <benjamin.park@nrel.gov>
2022-01-27 09:23:45 -0700

    Code cleanup

--- setup.py
+++ setup.py
@@ -38,8 +38,7 @@ setup(
     install_requires=[
         'lxml',
         'future',
-        'jsonschema==3.1.1',
-        'numpy'
+        'jsonschema==3.1.1'
     ],
     extras_require={
         'dev': [
